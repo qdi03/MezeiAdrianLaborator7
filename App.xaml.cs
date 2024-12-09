987a465a38b03cba2c8d91be4b8e071341976bfb
ADRIAN MEZEI-RACZ <adrian.mezei@stud.ubbcluj.ro>
2024-12-09 14:35:41 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace MezeiAdrianLaborator7
+﻿using System;
+using MezeiAdrianLaborator7.Data;
+using System.IO;
+
+namespace MezeiAdrianLaborator7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ Models/ShopList.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+namespace MezeiAdrianLaborator7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
